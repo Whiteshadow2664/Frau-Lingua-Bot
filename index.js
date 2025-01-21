084a28fde10406c83a3cbb86905f8262a0936e29
Ds777774 <sjemma819@gmail.com>
2025-01-21 20:53:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -225,13 +225,13 @@ for (const question of questionsToAsk) {
     .setTitle(`**${selectedLanguage.charAt(0).toUpperCase() + selectedLanguage.slice(1)} Vocabulary Quiz**`)
     .setDescription(
         `What is the English meaning of **"${question.word}"**?\n\n` +
-        `🇦 ${question.options[0]}\n\n` +
+        '${question.options[0]}\n\n` +
 
-        `🇧 ${question.options[1]}\n\n` +
+        `${question.options[1]}\n\n` +
 
-        `🇨 ${question.options[2]}\n\n` +
+        '${question.options[2]}\n\n` +
 
-        `🇩 ${question.options[3]}`
+        '${question.options[3]}`
     )
     .setColor(embedColors[selectedLanguage])
     .setFooter({ text: 'React with the emoji corresponding to your answer.' });
