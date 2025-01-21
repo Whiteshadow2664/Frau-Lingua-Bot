696ce9174c57ec8636ef3bc4806f955473975e2d
Ds777774 <sjemma819@gmail.com>
2025-01-21 20:34:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -231,8 +231,11 @@ for (const question of questionsToAsk) {
     .setDescription(
         `What is the English meaning of **"${question.word}"**?\n\n` +
         `🇦 ${question.options[0]}\n\n` +
+
         `🇧 ${question.options[1]}\n\n` +
+
         `🇨 ${question.options[2]}\n\n` +
+
         `🇩 ${question.options[3]}`
     )
     .setColor(embedColors[selectedLanguage])
