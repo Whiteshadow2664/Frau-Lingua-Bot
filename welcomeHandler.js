96d04f884c0745028a75dab31397b9fda441e614
Ds777774 <sjemma819@gmail.com>
2025-01-22 21:59:43 +0530

    Update welcomeHandler.js

--- welcomeHandler.js
+++ welcomeHandler.js
@@ -5,14 +5,10 @@ const handleMemberJoin = async (member) => {
     const guild = member.guild;
     const memberCount = guild.memberCount;
 
-    // Get the user's profile picture (small, rectangular size for "strip" effect)
-    const profilePicture = member.user.displayAvatarURL({ dynamic: true, size: 128 }); // Size 128px, should be rectangular
-
-    // Send a message in the "general" channel mentioning the user and showing their profile picture
+    // Send a message in the "general" channel mentioning the user
     const channel = guild.channels.cache.find(ch => ch.name === 'general');
     if (channel) {
         await channel.send(`Hello <@${member.id}>, welcome to the server! You are member **#${memberCount}**!`);
-        await channel.send({ content: '', files: [profilePicture] }); // Profile picture sent after the message
     } else {
         console.error("Channel not found: 'general'");
     }
