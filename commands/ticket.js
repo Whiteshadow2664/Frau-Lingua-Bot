e0c614a2d9c670b5f71f7dd291bcbe74a35e482b
Ds777774 <sjemma819@gmail.com>
2025-01-22 13:46:45 +0530

    Update ticket.js

--- commands/ticket.js
+++ commands/ticket.js
@@ -1,69 +1,73 @@
-// commands/ticket.js
 const { EmbedBuilder } = require('discord.js');
 
-// Ticket Command: Create a ticket
-async function createTicket(message) {
-    // Check if the user already has an open ticket
-    const existingTicket = message.guild.channels.cache.find(
-        (channel) => channel.name === `ticket-${message.author.id}`
+// Create Ticket Command
+module.exports.createTicket = async (message) => {
+    const user = message.author;
+    const guild = message.guild;
+
+    // Check if a ticket channel already exists for the user
+    const existingTicketChannel = guild.channels.cache.find(
+        (channel) => channel.name === `ticket-${user.id}`
     );
 
-    if (existingTicket) {
+    if (existingTicketChannel) {
         return message.channel.send('You already have an open ticket!');
     }
 
-    // Create the ticket channel
     try {
-        const ticketChannel = await message.guild.channels.create(`ticket-${message.author.id}`, {
-            type: 'text',
-            topic: `Ticket for ${message.author.tag}`,
+        // Create a new text channel for the ticket
+        const ticketChannel = await guild.channels.create({
+            name: `ticket-${user.id}`,
+            type: 'GUILD_TEXT',
+            topic: `Ticket for ${user.tag}`,
+            parent: 'YOUR_CATEGORY_ID', // Set a category ID where tickets should go
             permissionOverwrites: [
                 {
-                    id: message.guild.id, // Deny everyone permission to view the channel
-                    deny: ['VIEW_CHANNEL'],
+                    id: guild.id,
+                    deny: ['ViewChannel'],
                 },
                 {
-                    id: message.author.id, // Allow the user to view and send messages in the channel
-                    allow: ['VIEW_CHANNEL', 'SEND_MESSAGES'],
-                },
-                {
-                    id: '1330222964985303172', // Support role ID
-                    allow: ['VIEW_CHANNEL', 'SEND_MESSAGES'],
+                    id: user.id,
+                    allow: ['ViewChannel', 'SendMessages', 'ReadMessageHistory'],
                 },
             ],
         });
 
-        // Send an initial message in the ticket channel
-        await ticketChannel.send(
-            `Hello ${message.author.tag}, how can we assist you? Please describe your issue here. A staff member will respond soon.`
-        );
+        // Send a welcome message in the new ticket channel
+        const embed = new EmbedBuilder()
+            .setTitle('Ticket Created')
+            .setDescription(`Hello ${user.tag}, your ticket has been created.\nPlease describe your issue or request.`)
+            .setColor('#1cd86c')
+            .setFooter({ text: 'Ticket System' });
 
-        // Notify the user about the ticket creation
-        message.channel.send(`Your ticket has been created! Check <#${ticketChannel.id}> to describe your issue.`);
+        await ticketChannel.send({ embeds: [embed] });
+        message.channel.send(`Your ticket has been created! Go to ${ticketChannel} to provide more details.`);
     } catch (error) {
-        console.error('Error creating ticket channel:', error);
-        return message.channel.send('There was an error creating the ticket. Please try again later.');
+        console.error('Error creating ticket:', error);
+        message.channel.send('There was an error creating your ticket. Please try again later.');
     }
-}
+};
 
-// Ticket Command: Close a ticket
-async function closeTicket(message) {
-    // Ensure the user is either the ticket owner or a staff member
-    if (message.author.id !== message.channel.name.split('-')[1] && !message.member.roles.cache.has('1330222964985303172')) {
-        return message.channel.send('You are not authorized to close this ticket.');
+// Close Ticket Command
+module.exports.closeTicket = async (message) => {
+    const user = message.author;
+    const guild = message.guild;
+
+    // Find the ticket channel for the user
+    const ticketChannel = guild.channels.cache.find(
+        (channel) => channel.name === `ticket-${user.id}`
+    );
+
+    if (!ticketChannel) {
+        return message.channel.send('You do not have any open tickets.');
     }
 
-    // Close the ticket by deleting the channel
     try {
-        await message.channel.send('Closing the ticket...');
-        await message.channel.delete();
+        // Close the ticket by deleting the channel
+        await ticketChannel.delete();
+        message.channel.send(`The ticket has been closed. Thank you for reaching out, ${user.tag}.`);
     } catch (error) {
-        console.error('Error closing ticket channel:', error);
-        return message.channel.send('There was an error closing the ticket. Please try again later.');
+        console.error('Error closing ticket:', error);
+        message.channel.send('There was an error closing your ticket. Please try again later.');
     }
-}
-
-module.exports = {
-    createTicket,
-    closeTicket,
 };
