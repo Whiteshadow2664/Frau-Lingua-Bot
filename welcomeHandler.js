5db78a6ad4e57ddc5af5bc6ba67f59d6748b5c5f
Ds777774 <sjemma819@gmail.com>
2025-01-22 21:55:38 +0530

    Update welcomeHandler.js

--- welcomeHandler.js
+++ welcomeHandler.js
@@ -1,18 +1,18 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
 
 // Function to handle member joins
 const handleMemberJoin = async (member) => {
     const guild = member.guild;
     const memberCount = guild.memberCount;
 
-    // Get the user's profile picture URL (small and rectangular)
-    const profilePicture = member.user.displayAvatarURL({ dynamic: true, size: 128 }); // Size: 128px (small, rectangular format)
+    // Get the user's profile picture (small, rectangular size for "strip" effect)
+    const profilePicture = member.user.displayAvatarURL({ dynamic: true, size: 128 }); // Size 128px, should be rectangular
 
-    // Send a regular message mentioning the user and showing their profile picture
+    // Send a message in the "general" channel mentioning the user and showing their profile picture
     const channel = guild.channels.cache.find(ch => ch.name === 'general');
     if (channel) {
         await channel.send(`Hello <@${member.id}>, welcome to the server! You are member **#${memberCount}**!`);
-        await channel.send({ content: '', files: [profilePicture] }); // Profile picture (rectangular size)
+        await channel.send({ content: '', files: [profilePicture] }); // Profile picture sent after the message
     } else {
         console.error("Channel not found: 'general'");
     }
@@ -23,7 +23,7 @@ const handleMemberLeave = async (member) => {
     const guild = member.guild;
     const memberCount = guild.memberCount;
 
-    // Send a message mentioning the user and showing the updated member count
+    // Send a leave message mentioning the user and showing the updated member count
     const channel = guild.channels.cache.find(ch => ch.name === 'general');
     if (channel) {
         await channel.send(`${member.user.username} has left the server. We now have **${memberCount}** members.`);
