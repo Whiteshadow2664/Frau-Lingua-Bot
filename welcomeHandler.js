2f9d61feae5611fa604d1a2bd2be820d87902d3f
Ds777774 <sjemma819@gmail.com>
2025-01-22 21:49:24 +0530

    Update welcomeHandler.js

--- welcomeHandler.js
+++ welcomeHandler.js
@@ -6,13 +6,13 @@ const handleMemberJoin = async (member) => {
     const memberCount = guild.memberCount;
 
     // Get the user's profile picture URL (small and rectangular)
-    const profilePicture = member.user.displayAvatarURL({ dynamic: true, size: 128 }); // Size: 128px
+    const profilePicture = member.user.displayAvatarURL({ dynamic: true, size: 128 }); // Size: 128px (small, rectangular format)
 
     // Send a regular message mentioning the user and showing their profile picture
     const channel = guild.channels.cache.find(ch => ch.name === 'general');
     if (channel) {
         await channel.send(`Hello <@${member.id}>, welcome to the server! You are member **#${memberCount}**!`);
-        await channel.send({ content: '', files: [profilePicture] }); // Only the profile picture (rectangular size)
+        await channel.send({ content: '', files: [profilePicture] }); // Profile picture (rectangular size)
     } else {
         console.error("Channel not found: 'general'");
     }
@@ -26,7 +26,7 @@ const handleMemberLeave = async (member) => {
     // Send a message mentioning the user and showing the updated member count
     const channel = guild.channels.cache.find(ch => ch.name === 'general');
     if (channel) {
-        await channel.send(`<@${member.id}> has left the server. We now have **${memberCount}** members.`);
+        await channel.send(`${member.user.username} has left the server. We now have **${memberCount}** members.`);
     } else {
         console.error("Channel not found: 'general'");
     }
