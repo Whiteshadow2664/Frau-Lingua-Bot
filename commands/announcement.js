af804904b23cf9db7d2de5367c16822df7117994
Ds777774 <sjemma819@gmail.com>
2025-01-24 21:19:54 +0530

    Update announcement.js

--- commands/announcement.js
+++ commands/announcement.js
@@ -2,7 +2,7 @@ const { MessageActionRow, MessageButton } = require('discord.js');
 
 module.exports = {
   name: 'announcement',
-  description: 'Send an announcement to the general channel with @everyone ping',
+  description: 'Send an announcement to the general channel with everyone ping',
 
   async execute(message, args) {
     // Check if the message author has the 'Moderator' role
@@ -25,7 +25,7 @@ module.exports = {
       return message.reply('Announcements channel not found.');
     }
 
-    // Send the message to the 'announcements' channel with @everyone ping
+    // Send the message to the 'announcements' channel with everyone ping
     try {
       const sentMessage = await announcementChannel.send(`@everyone\n\n${announcementMessage}`);
 
