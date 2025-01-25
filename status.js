097715bb6ba57d3094c887434082351acd8f5d92
Ds777774 <sjemma819@gmail.com>
2025-01-25 19:10:45 +0530

    Update status.js

--- status.js
+++ status.js
@@ -1,1 +1,28 @@
-.
+module.exports = (client) => {
+  const statuses = [
+    'Hallo',
+    'Bonjour',
+    'Привет',
+    'Welcome to LinguaLounge',
+    "I'm Frau Lingua",
+    'Type !help',
+    'Type !quiz',
+    'Monitoring You',
+
+  ];
+
+  let i = 0;
+
+  setInterval(() => {
+    if (i >= statuses.length) i = 0; // Reset index if at the end of the statuses
+    const status = statuses[i];
+
+    // Set the bot's presence (custom status)
+    client.user.setPresence({
+      activities: [{ name: status }],
+      status: 'online', // Set the bot's online status (online, idle, dnd, invisible)
+    });
+
+    i++;
+  }, 3000); // Change status every 5 seconds
+};
