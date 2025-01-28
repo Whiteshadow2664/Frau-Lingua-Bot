db44dbbea9b31001c7734c9873752bdc1b5dc0ea
Ds777774 <sjemma819@gmail.com>
2025-01-28 22:55:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -126,7 +126,7 @@ Object.keys(wordOfTheDayTimes).forEach((language) => {
 
 // Leaderboard Scheduler
 cron.schedule(
-    '53 22 * * *',
+    '58 22 * * *',
     () => {
         const leaderboardChannelId = '1333119423711547414'; // Replace with actual channel ID
         messageTracker.generateLeaderboard(client, leaderboardChannelId);
