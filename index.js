243c3a64be4e3f263f5c767c70d9f574093db9dd
Ds777774 <sjemma819@gmail.com>
2025-01-30 19:19:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,6 @@ const leaderboard = require('./leaderboard.js');
 const linkFilter = require('./linkFilter');
 const { handleSpamDetection } = require('./spamHandler');
 const messageTracker = require('./messageTracker');
-const dmHandler = require('./dmHandler');
 
 // Environment Variables
 const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
@@ -384,7 +383,6 @@ delete activeQuizzes[message.author.id];
 client.once('ready', () => {
     console.log(`${client.user.tag} is online!`);
     linkFilter(client);
-    dmHandler(client);
 });
 
 // Event when a member joins the server
