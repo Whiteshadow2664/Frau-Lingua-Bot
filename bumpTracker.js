3bb3a4cbb3dd34a8fdbd0aab5e3326bf24e78ed4
Ds777774 <sjemma819@gmail.com>
2025-02-06 14:00:54 +0530

    Update bumpTracker.js

--- bumpTracker.js
+++ bumpTracker.js
@@ -52,7 +52,7 @@ const createTable = async () => {
 // Track Bumps
 const trackBump = async (message) => {
     if (
-        message.author.id === '540129267728515072' && // Disboard Bot ID
+        message.author.id === '735147814878969968' && // Disboard Bot ID
         message.content.includes('Thx for bumping our Server!')
     ) {
         const mentionedUser = message.mentions.users.first();
@@ -98,7 +98,7 @@ const showBumpLeaderboard = async (message) => {
                 .join('\n');
 
             const embed = new EmbedBuilder()
-                .setTitle('📌 Disboard Bump Leaderboard')
+                .setTitle('Disboard Bumps')
                 .setColor('#acf508')
                 .setDescription(`**Rank - Username - Bumps**\n${leaderboard}`)
                 .setFooter({ text: 'Keep bumping to climb the ranks!' });
