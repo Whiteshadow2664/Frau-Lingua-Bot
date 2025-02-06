a9bf6355eba8bdea0c5a4b5a66014fefe1c6efb0
Ds777774 <sjemma819@gmail.com>
2025-02-06 13:56:45 +0530

    Update welcomeHandler.js

--- welcomeHandler.js
+++ welcomeHandler.js
@@ -1,15 +1,31 @@
-const { EmbedBuilder } = require('discord.js');
+const { TextChannel, EmbedBuilder } = require('discord.js');
+
+// Function to send a message with retry logic
+async function sendMessageWithRetry(channel, messageContent) {
+    try {
+        await channel.send(messageContent);
+        console.log('Message sent successfully');
+    } catch (error) {
+        if (error.status === 503) {
+            console.error('Discord service unavailable, retrying...');
+            setTimeout(() => sendMessageWithRetry(channel, messageContent), 5000); // Retry after 5 seconds
+        } else {
+            console.error('Error sending message:', error);
+        }
+    }
+}
 
 // Function to handle member joins
 const handleMemberJoin = async (member) => {
     const guild = member.guild;
     const memberCount = guild.memberCount;
 
-   // Send a message in the "welcome" channel mentioning the user
-    const channel = guild.channels.cache.find(ch => ch.name === 'welcome');
-    if (channel) {
-        await channel.send(`Welcome to **LinguaLounge**, <@${member.id}>! We are excited to have you join our community. You are now member **#${memberCount}**. Enjoy your time here!`);
-    }
+    // Find the "welcome" channel
+    const channel = guild.channels.cache.find(ch => ch.name === 'welcome');
+    if (channel && channel instanceof TextChannel) {
+        const welcomeMessage = `Welcome to **LinguaLounge**, <@${member.id}>! We are excited to have you join our community. You are now member **#${memberCount}**. Enjoy your time here!`;
+        await sendMessageWithRetry(channel, welcomeMessage);
+    }
 };
 
 // Function to handle member leaves
@@ -17,10 +33,11 @@ const handleMemberLeave = async (member) => {
     const guild = member.guild;
     const memberCount = guild.memberCount;
 
-    // Send a leave message mentioning the user and showing the updated member count
+    // Find the "welcome" channel
     const channel = guild.channels.cache.find(ch => ch.name === 'welcome');
-    if (channel) {
-        await channel.send(`We're sorry to see you go, **${member.user.username}**. You will be missed. We now have **${memberCount}** members in **LinguaLounge**.`);
+    if (channel && channel instanceof TextChannel) {
+        const leaveMessage = `We're sorry to see you go, **${member.user.username}**. You will be missed. We now have **${memberCount}** members in **LinguaLounge**.`;
+        await sendMessageWithRetry(channel, leaveMessage);
     }
 };
 
