f9138ec5efee72ee5a6e30f5941bafefcf84f39b
Whiteshadow2664 <sjemma819@gmail.com>
2025-02-21 14:28:18 +0530

    Update antiInvite.js

--- antiInvite.js
+++ antiInvite.js
@@ -1,30 +1,40 @@
 const { Events } = require("discord.js");
 
 const warnedUsers = new Map(); // Track warnings
-const allowedChannelId = "1326458294994210877"; // Allowed channel for invites
+const allowedChannelId = "1342413437837377626"; // Allowed channel for invites
+const allowedServerId = "YOUR_SERVER_ID"; // Replace with your server ID
 
 module.exports = (client) => {
     client.on(Events.MessageCreate, async (message) => {
         if (message.author.bot || !message.guild) return;
 
-        const inviteRegex = /discord\.gg\/\w+/i; // Regex to detect invite links
+        const inviteRegex = /discord\.gg\/(\w+)/i; // Regex to detect invite links
+        const match = message.content.match(inviteRegex);
 
-        // Ignore if the message is in the allowed channel
-        if (message.channel.id === allowedChannelId) return;
-
-        if (inviteRegex.test(message.content)) {
+        if (match) {
             try {
+                const inviteCode = match[1];
+
+                // Fetch invite details to check if it's for your server
+                const invite = await client.fetchInvite(inviteCode).catch(() => null);
+
+                // Allow invites from the same server
+                if (invite && invite.guild.id === allowedServerId) return;
+
+                // Ignore if the message is in the allowed channel
+                if (message.channel.id === allowedChannelId) return;
+
                 await message.delete(); // Delete the invite message
-                
+
                 const userId = message.author.id;
                 const guildId = message.guild.id;
                 const key = `${guildId}-${userId}`; // Unique key for the user in the server
-                
+
                 if (!warnedUsers.has(key)) {
                     // First offense, send a warning
                     warnedUsers.set(key, 1);
                     return message.channel.send(
-                        `${message.author}, don't advertise your server! This isn't your dad's server. If you do this again, you'll be timed out.`
+                        `${message.author}, don't advertise other servers! This isn't your dad's server. If you do this again, you'll be timed out.`
                     );
                 } else {
                     // Second offense, timeout for 7 days
@@ -32,7 +42,7 @@ module.exports = (client) => {
                     if (member.moderatable) {
                         await member.timeout(7 * 24 * 60 * 60 * 1000, "Sent invite link twice.");
                         return message.channel.send(
-                            `${message.author} has been timed out for 7 days for advertising servers.`
+                            `${message.author} has been timed out for 7 days for advertising other servers.`
                         );
                     } else {
                         return message.channel.send(
