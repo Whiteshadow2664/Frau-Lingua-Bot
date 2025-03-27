27db2c676f9cf8582e0d1cb4b86e6f88be6ce657
Whiteshadow2664 <sjemma819@gmail.com>
2025-03-27 21:46:08 +0530

    Create joke.js

---
+++ commands/joke.js
@@ -0,0 +1,25 @@
+const axios = require("axios");
+const { EmbedBuilder } = require("discord.js");
+
+module.exports = {
+    name: "joke",
+    description: "Fetches a random joke from an API.",
+    execute: async (message) => {
+        try {
+            const response = await axios.get("https://official-joke-api.appspot.com/random_joke");
+            const joke = `**${response.data.setup}**\n\n*${response.data.punchline}*`;
+
+            const embed = new EmbedBuilder()
+                .setColor("#acf508")
+                .setTitle("📢 Random Joke")
+                .setDescription(joke)
+                .setFooter({ text: "Hope this made you smile!" })
+                .setTimestamp();
+
+            message.channel.send({ embeds: [embed] });
+        } catch (error) {
+            console.error("Error fetching joke:", error);
+            message.reply("Sorry, I couldn't fetch a joke at the moment.");
+        }
+    },
+};
