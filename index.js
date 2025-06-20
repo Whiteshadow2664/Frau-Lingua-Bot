07b1244146e0185a021b41434f3673bff13f655e
Whiteshadow2664 <sjemma819@gmail.com>
2025-06-20 09:51:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -46,6 +46,7 @@ const mailCommand = require('./commands/mail');
 const reactionCommand = require('./commands/reaction');
 const dateChannelUpdater = require('./dateChannel');
 
+const brankNoDB = require('./brankNoDB');
 
 
 // Environment Variables
@@ -175,6 +176,15 @@ if (message.content.toLowerCase() === '!exam') {
     examCommand.execute(message);
 }
 
+
+    
+if (message.content === '!brank') {
+        await brankNoDB.execute(message);
+}
+    
+
+
+    
 if (message.content.toLowerCase() === '!tips') {
     studyTips.execute(message);
 }
