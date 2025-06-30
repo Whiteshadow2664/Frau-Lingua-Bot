9c5d3db7e51d0724d531e25cea8b39e976e06361
Whiteshadow2664 <sjemma819@gmail.com>
2025-06-30 10:23:18 +0530

    Create mediaBlocker.js

---
+++ blacklist/mediaBlocker.js
@@ -0,0 +1,23 @@
+// mediaBlocker.js
+const { Events } = require('discord.js');
+
+// Replace with your actual role ID (users WITH this role can't send media)
+const MEDIA_BLOCK_ROLE_ID = 'ROLE_ID_FOR_MEDIA_BLOCK';
+
+module.exports = {
+    monitorMedia: (client) => {
+        client.on(Events.MessageCreate, async (message) => {
+            if (message.author.bot || !message.guild) return;
+
+            const member = message.member;
+            if (!member || !member.roles.cache.has(MEDIA_BLOCK_ROLE_ID)) return;
+
+            const hasMedia = message.attachments.size > 0 || message.embeds.some(embed => embed.image || embed.video);
+            if (hasMedia) {
+                await message.delete().catch(console.error);
+                message.channel.send(`🚫 <@${message.author.id}> you're not allowed to send media.`)
+                    .then(msg => setTimeout(() => msg.delete().catch(() => {}), 5000));
+            }
+        });
+    }
+};
