158cf206800119dd86ddbc159c784eb0647cb6f3
Whiteshadow2664 <sjemma819@gmail.com>
2025-06-30 11:08:21 +0530

    Update linkBlocker.js

--- blacklist/linkBlocker.js
+++ blacklist/linkBlocker.js
@@ -1,9 +1,6 @@
 // linkBlocker.js
 const { Events } = require('discord.js');
-
-// Replace with your actual role ID (users WITH this role can't send links)
 const LINK_BLOCK_ROLE_ID = '1389103629964804267';
-
 const LINK_REGEX = /(https?:\/\/[^\s]+)/i;
 
 module.exports = {
@@ -15,9 +12,18 @@ module.exports = {
             if (!member || !member.roles.cache.has(LINK_BLOCK_ROLE_ID)) return;
 
             if (LINK_REGEX.test(message.content)) {
-                await message.delete().catch(console.error);
-                message.channel.send(` <@${message.author.id}> you're not allowed to send links.`)
-                    .then(msg => setTimeout(() => msg.delete().catch(() => {}), 300000));
+                try {
+                    await message.delete();
+                } catch (err) {
+                    console.error('❌ Failed to delete link message:', err.message);
+                }
+
+                try {
+                    const warnMsg = await message.channel.send(` <@${message.author.id}> you're not allowed to send links.`);
+                    setTimeout(() => warnMsg.delete().catch(() => {}), 5000); // safer timeout
+                } catch (err) {
+                    console.error('❌ Failed to send/delete warning message:', err.message);
+                }
             }
         });
     }
