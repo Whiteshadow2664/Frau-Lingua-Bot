73f80b6280e1a61334b86ce19422fe4ef5772f6a
Whiteshadow2664 <sjemma819@gmail.com>
2025-12-25 13:28:58 +0530

    Update festivals.js

--- festivals.js
+++ festivals.js
@@ -98,7 +98,7 @@ function getFestivalData() {
 
 module.exports = (client) => {
     cron.schedule(
-        "14 13 * * *", // runs daily at 09:00 IST
+        "34 13 * * *", // runs daily at 09:00 IST
         async () => {
             const todayFull = new Date().toLocaleDateString("en-CA", { timeZone: "Asia/Kolkata" });
 
